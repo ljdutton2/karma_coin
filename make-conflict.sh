f90f1d3e02bf7bbe1d488200f32a6c11ae611c6c
inv0w <jonjoninfante@gmail.com>
2020-02-03 10:16:44 -0800

    typos commit

--- make-conflict.sh
+++ make-conflict.sh
@@ -1,11 +1,4 @@
-touch my_code.sh
-git add my_code.sh
-echo "echo Hello" > my_code.sh
-git commit -am 'initial'
+git commit -am 'typos commit'
 git checkout typos
-echo "echo \"Hello World\"" > my_code.sh
-git commit -am 'first commit on typos'
-git checkout typos
-echo "echo \"Hello World!\"" > my_code.sh
-git commit -am 'second commit on master'
+git commit -am 'merging dev into typos'
 git merge dev
