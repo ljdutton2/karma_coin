054f4f595eae4cd60b4f58b7291e9d81796f4be9
Muhammad Tifak <moht20009@gmail.com>
2020-02-24 11:50:38 -0800

    Finally able to call one food item

--- app.py
+++ app.py
@@ -26,6 +26,8 @@ SPOONACULAR_API_KEY = os.getenv("SPOONACULAR_API_KEY")
 # Retrieves raw scores
 app = Flask(__name__)
 
+
+##### GOOGLE VISION API #####
 url = 'https://vision.googleapis.com/v1/images:annotate?'
 payload = open("google_vision.json")
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
@@ -34,7 +36,22 @@ params = {
     }
 r = requests.post(url, data=payload, headers=headers, params=params)
 
+labels = ['Food', 'Junk food', 'Dish', 'Cuisine', 'Fast food', 'Ingredient', 'Meat', 'Produce',
+'Dessert', 'Frozen dessert', 'Baked goods', 'Comfort food', 'Staple food', 'Recipe', 'Italian food',
+'Vegetarian food', 'American food', 'Vegetable', 'Plant', 'Natural foods', 'Fruit', 'Superfood',
+'Red', 'Banana family', 'Yellow', 'Citrus', 'Citric acid', 'Side dish', 'Local food', 'Juice',
+'Drink', 'Orange drink', 'Breakfast', 'Meal', 'Lunch', 'Dinner', 'Bilberry', 'Hendl', 'Kai yang'
+'Roasting', 'Green', 'Leaf', 'Corn kernels', 'Animal fat', 'Crocus', 'Flower', 'Nut', 'Leaf vegetable',
+'Herb', 'Grass']
 
+print(r.json())
+#This returns the first label/tag based on topicality/closeness of what the image looks like
+if r.json()['responses'][0]['labelAnnotations'][0]['description'] not in labels:
+    food_item = r.json()['responses'][0]['labelAnnotations'][0]['description']
+
+print(food_item)
+
+##### SPOONACULAR API #####
 url_s = 'https://api.spoonacular.com/recipes/findByIngredients'
 params_s = {
     'apiKey': SPOONACULAR_API_KEY,
@@ -54,22 +71,6 @@ s_recipe = requests.get(url_s2, params=params_s2)
 print(s_recipe.json()[0]['steps'][0]['step'])
 
 
-
-labels = ['Food', 'Junk food', 'Dish', 'Cuisine', 'Fast food', 'Ingredient', 'Meat', 'Produce',
-'Dessert', 'Frozen dessert', 'Baked goods', 'Comfort food', 'Staple food', 'Recipe', 'Italian food',
-'Vegetarian food', 'American food', 'Vegetable', 'Plant', 'Natural foods', 'Fruit', 'Superfood',
-'Red', 'Banana family', 'Yellow', 'Citrus', 'Citric acid', 'Side dish', 'Local food', 'Juice',
-'Drink', 'Orange drink', 'Breakfast', 'Meal', 'Lunch', 'Dinner', 'Bilberry', 'Hendl', 'Kai yang'
-'Roasting', 'Green', 'Leaf', 'Corn kernels', 'Animal fat', 'Crocus', 'Flower', 'Nut', 'Leaf vegetable',
-'Herb', 'Grass']
-
 @app.route('/')
 def home_page():
     return render_template("RecipEat/base.html", stuff=r.json()['responses'][0]['labelAnnotations'][i]['description'], things=labels)
-
-# print(r.json())
-#
-#
-# for i in range(10):
-#     if r.json()['responses'][0]['labelAnnotations'][i]['description'] not in labels:
-#         print(r.json()['responses'][0]['labelAnnotations'][i]['description'])
