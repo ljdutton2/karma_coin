57868eb872527825bee53f205f31798fa03ea205
lauren <ljdutton2@icloud.com>
2020-10-13 17:41:13 -0500

    remove react boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+// import drizzle functions and contract artifact
+import { Drizzle, generateStore } from "@drizzle/store";
+import Greeter from "./artifacts/Greeter.json";
+// let drizzle know what contracts we want
+const options = { contracts: [Greeter] };
+// setup the drizzle store and drizzle
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
+// pass in the drizzle instance
+ReactDOM.render(<App drizzle={drizzle} />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
